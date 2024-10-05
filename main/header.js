7374c6f04caa162bdc7b271de44fa5cf32c5a4fe
demesup <guzhansofia@gmail.com>
2024-10-05 19:53:47 +0200

    reorganization

---
+++ main/header.js
@@ -0,0 +1,14 @@
+const header = `
+    <div class="logo">
+        <a href="/main/index.html">
+            <h1>P5.js Projects</h1>
+        </a>
+    </div>
+    <nav>
+        <ul>
+            <li><a href="/main/pages/sand.html">Sand Drop</a></li>
+            <li><a href="/main/pages/game-of-life.html">Game of life</a></li>
+            <li><a href="/main/pages/hearts.html">Hearts</a></li>
+        </ul>
+    </nav>
+</>`
